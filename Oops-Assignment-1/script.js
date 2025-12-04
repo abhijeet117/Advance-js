5ba0add939e23836d53496109ac0139f406ae64e
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-12-04 13:39:43 +0530

    first commit

---
+++ Oops-Assignment-1/script.js
@@ -0,0 +1,18 @@
+// SECTION 1: Objects and OOPS Thinking (Foundation)
+
+// 	1.	Create a user object that stores name and email
+//  and has a login method which prints “User logged in”.
+
+
+
+
+// 	2.	Imagine you now have 5 users.
+// First, think how you would manage them without using a class.
+// Then convert the same logic using a class and observe
+// how the code becomes cleaner. Write code for both approaches.
+// 	3.	Create a product object that stores name and 
+// price and has a method which returns the final price after discount.
+
+// The goal of this section is to understand why keeping data and behavior together makes code easier to manage.
+ 
+
