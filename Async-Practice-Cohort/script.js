a9789f1e4e900c5bf4de9eaddcf273bab37c3e43
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-12-09 20:41:39 +0530

    solved ex-3

--- Async-Practice-Cohort/script.js
+++ Async-Practice-Cohort/script.js
@@ -81,3 +81,67 @@ getUser("@abhijeet",function(user) {
 }); */
 
 
+// ## Exercise 3 — Intermediate (Callback dependency — thoda painful)
+
+// **Task (Hindi):** Teen functions banao:
+
+// 1. `loginUser`
+//    - 1 second baad callback ko `user` object de
+// 2. `fetchPermissions`
+//    - `userId` lega
+//    - 1 second baad callback ko `permissions` array de
+// 3. `loadDashboard`
+//    - `permissions` lega
+//    - 1 second baad callback ko `"Dashboard loaded"` bole
+
+// **Flow:**
+// - Pehle `loginUser`
+// - Uske andar `fetchPermissions`
+// - Uske andar `loadDashboard`
+// - Final output console mein print ho
+
+// **Goal:**
+// - Callback nesting ko feel karna
+// - Yehi structure baad mein callback hell banta hai
+
+/* function loginUser(user, cb) {
+    console.log("loging user...");
+    setTimeout(function(){
+        cb({user: user});
+    },1000);
+}
+
+function fetchPermissions(userid, cb) {
+    console.log("fetching permission...");
+    setTimeout(function(){
+        cb(["read", "write", "delete"]);
+    },1000);
+}
+
+function loadDashboard(permission, cb) {
+    console.log("loading dashboard...");
+    setTimeout(function(){
+        cb();
+    },1000);
+}
+
+loginUser("abhi",function(data) {
+    fetchPermissions(data.user, function(permissions) {
+        loadDashboard(permissions, function() {
+            console.log("Dashboard loaded");
+        });
+    });
+});
+
+// ### Notes
+// - Practice in plain JavaScript using `setTimeout` and callbacks
+//  to understand the flow before converting to Promises/async–await.
+
+
+
+
+
+
+
+
+
