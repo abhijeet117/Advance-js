9676897046d565915fe1e41f837958fd768568f5
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-12-09 20:38:28 +0530

    solved exercise 1

---
+++ Async-Practice-Cohort/script.js
@@ -0,0 +1,43 @@
+// # Day 60 — Exercises
+
+// ---
+
+// ## Exercise 1 — Very Easy (Warming up)
+
+// **Task (Hindi):** Ek function banao `afterDelay`
+
+// **Requirements:**
+// - Ye function do cheezein lega:
+//   1. `time` (milliseconds)
+//   2. `callback` function
+// - Given `time` ke baad `callback` call kare
+// - Callback ke andar `"Callback executed"` print hona chahiye
+
+// **Use case:**
+// > “2 second baad ek kaam karna hai”
+
+// **Goal:**
+// - Samajhna ki callback delay ke baad kaise execute hota hai
+// - Ye `setTimeout` + callback connection hai
+
+/* function afterDelay(time, cb) {
+    console.log("Callback executing...")
+    setTimeout(function() {
+        cb({callback: "Callback executed"});
+    },time)
+}
+
+afterDelay(1000, function(data) {
+    console.log(data.callback)
+}); */
+
+
+
+
+
+
+
+
+
+
+
