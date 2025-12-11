cb9168c8a921793e92890373e4a5115a48060af3
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-12-11 09:34:33 +0530

    solved 2nd question

--- Promises-Practice-Qs/script.js
+++ Promises-Practice-Qs/script.js
@@ -19,3 +19,56 @@ foodDelivery()
   .catch(function () {
     console.log("Delivery failed!");
   }); */
+
+// 2. chained promises: user -- posts -- comments
+// a. create getUser() - resolves with {id: 1 , name "abhij"}
+// b. getPost(userId) - resolves with list of poast titles
+// c. getComments(postId) - resolves with comments
+// chain them together using .then() and log the final output
+
+/* function getUser() {
+  console.log("getting user info...");
+  return new Promise(function (res, rej) {
+    setTimeout(() => {
+      res({ id: 1, name: "abhij" });
+    }, 2000);
+  });
+}
+
+function getPost() {
+  console.log("getting user posts...");
+  return new Promise(function (res, rej) {
+    setTimeout(() => {
+      res(["a nice day", "glowing up", "wether is fine"]);
+    }, 1000);
+  });
+}
+
+function getComments() {
+  console.log("getting user comments...");
+  return new Promise(function (res, rej) {
+    setTimeout(() => {
+      res(["wowo!", "hii", "angel priya!"]);
+    }, 1500);
+  });
+}
+
+getUser()
+  .then(function (data) {
+    console.log(data);
+    return getPost(data.id);
+  })
+  .then(function (titles) {
+    console.log(titles);
+    return getComments("kuch comment");
+  })
+  .then(function (cmts) {
+    console.log(cmts);
+  })
+  .finally(function () {
+    console.log("All Conditions are now fullfilled!");
+  }); */
+
+
+
+
